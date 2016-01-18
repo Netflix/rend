cdf965423b424a8972a086ccbc3737edb84a51e8
Scott Mansfield <sgmansf@gmail.com>
2016-01-18 15:36:56 -0800

    Remove unnecessary init

--- common/datatypes.go
+++ common/datatypes.go
@@ -21,18 +21,18 @@ package common
 import "errors"
 
 var (
-	BAD_LENGTH error
-	BAD_FLAGS  error
+	BAD_LENGTH = errors.New("CLIENT_ERROR length is not a valid integer")
+	BAD_FLAGS = errors.New("CLIENT_ERROR flags is not a valid integer")
 
-	ERROR_KEY_NOT_FOUND     error
-	ERROR_KEY_EXISTS        error
-	ERROR_VALUE_TOO_BIG     error
-	ERROR_INVALID_ARGS      error
-	ERROR_ITEM_NOT_STORED   error
-	ERROR_BAD_INC_DEC_VALUE error
-	ERROR_AUTH_ERROR        error
-	ERROR_UNKNOWN_CMD       error
-	ERROR_NO_MEM            error
+	ERROR_KEY_NOT_FOUND = errors.New("ERROR Key not found")
+	ERROR_KEY_EXISTS = errors.New("ERROR Key already exists")
+	ERROR_VALUE_TOO_BIG = errors.New("ERROR Value too big")
+	ERROR_INVALID_ARGS = errors.New("ERROR Invalid arguments")
+	ERROR_ITEM_NOT_STORED = errors.New("ERROR Item not stored (CAS didn't match)")
+	ERROR_BAD_INC_DEC_VALUE = errors.New("ERROR Bad increment/decrement value")
+	ERROR_AUTH_ERROR = errors.New("ERROR Authentication error")
+	ERROR_UNKNOWN_CMD = errors.New("ERROR Unknown command")
+	ERROR_NO_MEM = errors.New("ERROR Out of memory")
 )
 
 // Make sure to keep this list in sync with the one above
@@ -129,19 +129,4 @@ type Metadata struct {
 	ChunkSize uint32
 	// This size should stay the same as local.TOKEN_SIZE
 	Token [16]byte
-}
-
-func init() {
-	BAD_LENGTH = errors.New("CLIENT_ERROR length is not a valid integer")
-	BAD_FLAGS = errors.New("CLIENT_ERROR flags is not a valid integer")
-
-	ERROR_KEY_NOT_FOUND = errors.New("ERROR Key not found")
-	ERROR_KEY_EXISTS = errors.New("ERROR Key already exists")
-	ERROR_VALUE_TOO_BIG = errors.New("ERROR Value too big")
-	ERROR_INVALID_ARGS = errors.New("ERROR Invalid arguments")
-	ERROR_ITEM_NOT_STORED = errors.New("ERROR Item not stored (CAS didn't match)")
-	ERROR_BAD_INC_DEC_VALUE = errors.New("ERROR Bad increment/decrement value")
-	ERROR_AUTH_ERROR = errors.New("ERROR Authentication error")
-	ERROR_UNKNOWN_CMD = errors.New("ERROR Unknown command")
-	ERROR_NO_MEM = errors.New("ERROR Out of memory")
 }
