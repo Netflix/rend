90d0241f94cb45a7abbe17f281e5a713e78f020d
Scott Mansfield <sgmansf@gmail.com>
2016-02-01 12:25:59 -0800

    Adding auto-registered http endpoint to get counter metrics

--- metrics/endpoint.go
+++ metrics/endpoint.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+const metricPrefix = "rend_"
+
 func init() {
 	http.Handle("/metrics/counters", http.HandlerFunc(printCounters))
 }
@@ -13,6 +15,6 @@ func printCounters(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	ctrs := GetAllCounters()
 	for name, val := range ctrs {
-		fmt.Fprintf(w, "%s %d\n", name, val)
+		fmt.Fprintf(w, "%s%s %d\n", metricPrefix, name, val)
 	}
 }
