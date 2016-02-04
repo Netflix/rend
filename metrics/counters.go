9d1c8b12ea4ee68107e567e56fb39b6a94477f8b
Scott Mansfield <sgmansf@gmail.com>
2016-02-04 11:13:37 -0800

    Lock-free counter implementation

--- metrics/counters.go
+++ metrics/counters.go
@@ -1,51 +1,43 @@
 package metrics
 
-import (
-	"sync"
-	"sync/atomic"
-)
+import "sync/atomic"
+
+const numMetrics = 1024
 
 var (
-	names    = make([]string, 0)
-	counters = make([]*uint64, 0)
-	lock     = new(sync.RWMutex)
+	names    = make([]string, numMetrics)
+	counters = make([]*uint64, numMetrics)
+	curID    = new(uint32)
 )
 
-// Registers a counter and returns an ID that can be used to access it
-func AddCounter(name string) int {
-	lock.Lock()
-	defer lock.Unlock()
-
-	counterID := len(names)
-
-	names = append(names, name)
-	counters = append(counters, new(uint64))
-
-	return counterID
+func init() {
+	// start with "-1" so the first metric ID overflows to 0
+	atomic.StoreUint32(curID, 0xFFFFFFFF)
 }
 
-func IncCounter(id int) {
-	lock.RLock()
-	defer lock.RUnlock()
+// Registers a counter and returns an ID that can be used to access it
+// There is a maximum of 1024 metrics, after which adding a new one will panic
+func AddCounter(name string) uint32 {
+	id := atomic.AddUint32(curID, 1)
+	names[id] = name
+	counters[id] = new(uint64)
+	return id
+}
 
+func IncCounter(id uint32) {
 	atomic.AddUint64(counters[id], 1)
 }
 
-func IncCounterBy(id int, amount uint64) {
-	lock.RLock()
-	defer lock.RUnlock()
-
+func IncCounterBy(id uint32, amount uint64) {
 	atomic.AddUint64(counters[id], amount)
 }
 
 func GetAllCounters() map[string]uint64 {
-	lock.RLock()
-	defer lock.RUnlock()
-
 	ret := make(map[string]uint64)
+	numIDs := int(atomic.LoadUint32(curID))
 
-	for i, name := range names {
-		ret[name] = atomic.LoadUint64(counters[i])
+	for i := 0; i < numIDs; i++ {
+		ret[names[i]] = atomic.LoadUint64(counters[i])
 	}
 
 	return ret
