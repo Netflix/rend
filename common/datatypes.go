21546aef84a372b3e4ee7ed1354de661e2999a3e
Scott Mansfield <sgmansf@gmail.com>
2016-02-10 18:33:01 -0800

    Commenting up the common package

--- common/datatypes.go
+++ common/datatypes.go
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package common contains all protocol-agnostic types and interfaces, as well as some variables
+// used by multiple other packages in the application. Metrics are useful in many places for
+// tracking bytes written and read. Errors exist here to be the neutral representation of either
+// protocol errors or other application errors that happen all over. Other interfaces and types used
+// by multiple protocols are here so any protocol can be used on the front end as well as the back
+// with the same intermediate representation.
 package common
 
 import (
@@ -20,8 +26,8 @@ import (
 	"github.com/netflix/rend/metrics"
 )
 
+// Common metrics used across packages
 var (
-	// Common metrics used across packages
 	MetricBytesReadRemote     = metrics.AddCounter("bytes_read_remote")
 	MetricBytesReadLocal      = metrics.AddCounter("bytes_read_local")
 	MetricBytesReadLocalL1    = metrics.AddCounter("bytes_read_local_l1")
@@ -52,9 +58,10 @@ var (
 	ErrTempFailure    = errors.New("ERROR Temporary error")
 )
 
-// Make sure to keep this list in sync with the one above
-// It should contain all Err* that could come back from
-// memcached itself
+// IsAppError differentiates between protocol-defined errors that are relatively benign and other
+// fatal errors like an IO error because of some socket problem or network issue.
+// Make sure to keep this list in sync with the one above. It should contain all Err* that could
+// come back from memcached itself
 func IsAppError(err error) bool {
 	return err == ErrKeyNotFound ||
 		err == ErrKeyExists ||
@@ -71,23 +78,43 @@ func IsAppError(err error) bool {
 		err == ErrTempFailure
 }
 
-// Request types for all protocols
+// RequestType is the protocol-agnostic identifier for the command
 type RequestType int
 
 const (
+	// RequestUnknown means the parser doesn't know what the request represents. Valid protocol
+	// parsing but invalid values.
 	RequestUnknown = iota
+
+	// RequestGet represents both a single get and a multi-get, which take differen forms in
+	// different protocols. This means it can also be the accumulation of many GETQ commands.
 	RequestGet
-	RequestGetQ
+
+	// RequestGat is a get-and-touch operation that retrieves the information while updating the TTL
 	RequestGat
+
+	// RequestSet is to insert a new piece of data unconditionally. What that means is different
+	// depending on L1 / L2 handling.
 	RequestSet
+
+	// RequestDelete deletes a piece of data from all levels of cache
 	RequestDelete
+
+	// RequestTouch updates the TTL for the item specified to a new TTL
 	RequestTouch
 )
 
+// RequestParser represents an interface to parse incoming requests. Each protocol provides its own
+// implementation. The return value is an interface{}, but not all hope is lost. The return result
+// is guaranteed by implementations to be castable to the type that matches the RequestType returned.
 type RequestParser interface {
 	Parse() (interface{}, RequestType, error)
 }
 
+// Responder is the interface for a protocol to respond to different commands. It responds in
+// whatever way is appropriate, including doing nothing or panic()-ing for unsupported interactions.
+// Unsupported interactions are OK to panic() on because they should never be returned from the
+// corresponding RequestParser.
 type Responder interface {
 	Set(opaque uint32) error
 	Get(response GetResponse) error
@@ -97,9 +124,11 @@ type Responder interface {
 	GATMiss(response GetResponse) error
 	Delete(opaque uint32) error
 	Touch(opaque uint32) error
-	Error(opaque uint32, err error) error
+	Error(opaque uint32, reqType RequestType, err error) error
 }
 
+// SetRequest corresponds to common.RequestSet. It contains all the information required to fulfill
+// a set request.
 type SetRequest struct {
 	Key     []byte
 	Data    []byte
@@ -108,7 +137,9 @@ type SetRequest struct {
 	Opaque  uint32
 }
 
-// Gets are batch by default
+// GetRequest corresponds to common.RequestGet. It contains all the information required to fulfill
+// a get requestGets are batch by default, so single gets and batch gets are both represented by the
+// same type.
 type GetRequest struct {
 	Keys       [][]byte
 	Opaques    []uint32
@@ -117,23 +148,32 @@ type GetRequest struct {
 	NoopEnd    bool
 }
 
+// DeleteRequest corresponds to common.RequestDelete. It contains all the information required to
+// fulfill a delete request.
 type DeleteRequest struct {
 	Key    []byte
 	Opaque uint32
 }
 
+// TouchRequest corresponds to common.RequestTouch. It contains all the information required to
+// fulfill a touch request.
 type TouchRequest struct {
 	Key     []byte
 	Exptime uint32
 	Opaque  uint32
 }
 
+// GATRequest corresponds to common.RequestGat. It contains all the information required to fulfill
+// a get-and-touch request.
 type GATRequest struct {
 	Key     []byte
 	Exptime uint32
 	Opaque  uint32
 }
 
+// GetResponse is used in both RequestGet and RequestGat handling. Both respond in the same manner
+// but with different opcodes. It is binary-protocol specific, but is still a part of the interface
+// of responder to make the handling code more protocol-agnostic.
 type GetResponse struct {
 	Key    []byte
 	Data   []byte
