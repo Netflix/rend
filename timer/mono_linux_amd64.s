42a5a29e6bc5ad5e8de3eedc7afd35c33124a928
Scott Mansfield <sgmansf@gmail.com>
2016-09-08 21:18:16 -0700

    Updated the source to use CLOCK_MONOTONIC with an implementation from the standard library.
    At this point there was no Spacemonkeygo code left, so I removed the copyright headers from the timer package and the license from the NOTICE file.
    I also added the Go license in there because I lifted the assembly of runtime.nanotime to get what I needed.

--- timer/mono_linux_amd64.s
+++ timer/mono_linux_amd64.s
@@ -1,10 +1,10 @@
-// Copyright (C) 2014 Space Monkey, Inc.
+// Copyright 2016 Netflix, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,29 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-TEXT ·monotime(SB), 7, $16
+// This function is borrowed from the Go standard library
+// Copyright (c) 2009 The Go Authors. All rights reserved.
+// See the NOTICE file for more details.
+TEXT ·Now(SB), 7, $16
 	MOVQ runtime·__vdso_clock_gettime_sym(SB), AX
-	CMPQ AX, $0
-	JEQ  vdso_is_sad
-	MOVL $4, DI                                   // CLOCK_MONOTONIC_RAW
-	LEAQ 0(SP), SI
-	CALL AX
-	MOVQ 0(SP), AX
-	MOVQ 8(SP), DX
-	MOVQ AX, sec+0(FP)
-	MOVL DX, nsec+8(FP)
-	RET
-
-vdso_is_sad:
-	MOVQ $0, sec+0(FP)
-	RET
-
-// This function is derived from the Go standard library under BSD license,
-// Copyright The Go Authors. See NOTICE file for more details
-
-TEXT ·Now(SB), 4, $16
-	MOVQ runtime·__vdso_clock_gettime_sym(SB), AX
-	MOVL $4, DI                                   // CLOCK_MONOTONIC_RAW
+	MOVL $1, DI                                   // CLOCK_MONOTONIC
 	LEAQ 0(SP), SI
 	CALL AX
 	MOVQ 0(SP), AX                                // sec
